22e0e41c18443aa2df0cfa4b082e904b40255fd7
Andris Saudinis <andris@picolab.lv>
2016-09-12 13:49:33 +0300

    Main slaider and represent content carousel

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,5 +2,18 @@ $(function() {
     $('.ui.dropdown').dropdown();
 
     $('.main-slider').bxSlider();
+
+    $('.represent-slider').bxSlider({
+        slideWidth: 190,
+        minSlides: 2,
+        maxSlides: 6,
+        moveSlides: 2,
+        slideMargin: 0,
+        controls: false,
+        pager: false,
+        infiniteLoop: true,
+        auto: true,
+        autoControls: false
+    });
 })
 
