351d3ca34a3b7b9bfd15a940d40cc8ab7fadad56
Andris Saudinis <andris@picolab.lv>
2016-09-15 01:24:39 +0300

    mobile menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,18 @@
 $(function() {
-    $('.ui.dropdown').dropdown();
+    $('.menu').on('click', '.ui.dropdown', function (e) {
+        e.preventDefault();
+        $('.ui.dropdown').removeClass('active');
+        $(this).addClass('active');
+        if($(this).children('.menu').css('display')=='block') {
+            $(this).children('.menu').hide();
+            $(this).removeClass('active');
+            return;
+        }
+
+        $('.ui.dropdown .menu').hide();
+        $(this).children('.menu').slideToggle(200);
+    })
+    $('select.ui.dropdown').dropdown();
 
     $('.ui.rating').rating({
         maxRating: 5
@@ -21,5 +34,19 @@ $(function() {
         auto: true,
         autoControls: false
     });
+
+    $('.mob-search').on('click', function (e) {
+        e.preventDefault();
+        $('.search-row').slideToggle();
+        if ($('.search-row').css('display') == 'flex') {
+            $('.search-row input').focus();
+        }
+    })
+    $('.mob-toogle-menu').on('click', function (e) {
+        e.preventDefault();
+        $('.mob-menu').slideToggle(200);
+        $(this).children('i').toggleClass('icon-mob-toogle-close', 'icon-mob-toogle-open');
+        $(this).children('i').toggleClass('icon-mob-toogle-open');
+    })
 })
 
