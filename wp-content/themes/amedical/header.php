fd640e7b54348c008b23405d15c45c494557c74c
Ronald Wilson <ronaldwilson2005@gmail.com>
2016-09-14 18:44:44 +0300

    no message

--- wp-content/themes/amedical/header.php
+++ wp-content/themes/amedical/header.php
@@ -4,37 +4,27 @@
 		<?php include('includes/parts/head_common.php'); ?>
 	</head>
 	<body <?php body_class(); ?>>
-
-	<nav class="mobile-menu">
-
-    				<div class="menu-toggle">
-    					<span></span>
-    					<span></span>
-    					<span></span>
-    				</div>
-
-    				<?php wp_nav_menu(array('theme_location' => 'main-menu', 'menu_id' => 'mobile-menu', 'menu_class' => 'navigation-mobile', 'walker' => new Mobile_Menu_walker())); ?>
-    </nav>
-
-	<header>
-			<div class="language-switcher">
-    			<?php dswp_print_language_menu(); ?>
-    		</div>
-
-			<div class="row column">
-
-					<div class="text-center">
-					<a href="<?php echo esc_url(home_url()); ?>" class="logo">
-							<img src="<?php echo get_stylesheet_directory_uri().'/images/logo.svg'; ?>"
-									 alt="<?php echo esc_attr(get_bloginfo('name')); ?>">
-					</a>
-					<h1>Digital Score Wordpress Framework</h1>
-					</div>
-
-					<?php wp_nav_menu(array('theme_location' => 'main-menu','menu_id' => 'main-menu','menu_class' => 'navigation-primary')); ?>
-			</div>
-	</header>
-
-		<div class="content-wrapper">
-
-    <?php include('includes/parts/breadcrumbs.php'); ?>
+<header>
+  <div class="ui container relaxed grid">
+    <div class="ui two wide column"><a class="logo" href="index.html"><img src="img/logo.png"></a></div>
+    <div class="ui fourteen wide column">
+      <div class="ui text menu">
+        <div class="left menu">
+          <div class="item">
+            <div class="ui icon input search">
+              <input placeholder="Meklēt"><i class="search icon"></i>
+            </div>
+          </div><a class="item" href="content.html">Jaunumi</a><a class="item" href="#">Blogs</a><a class="item" href="#">Biežāk uzdotie jautājumi</a><a class="item" href="about-us.html">Par mums</a><a class="item" href="contact-us.html">Kontakti</a>
+        </div>
+        <div class="right menu lang"><a class="item active" href="#">LV</a><a class="item" href="#">EN</a><a class="item" href="#">RU</a></div>
+        <div class="right menu auth"><a href="#"><span class="ui basic small button">Autorizācija</span></a></div>
+      </div>
+      <nav class="ui secondary menu"><a class="item" href="index.html"><i class="icon-home-icon"></i></a><a class="item" href="content.html">Medicīnas iekārtas</a>
+        <div class="ui simple dropdown item">Medicīnas preces
+          <div class="menu"><a class="item" href="#">Medicīnas preces</a><a class="item" href="#">Instrumenti un aprīkojums</a></div>
+        </div><a class="item" href="#">Apģērbi un aizsarglīdzekļi</a><a class="item" href="projects.html">Projektēšana</a><a class="item" href="services.html">Iekārtu serviss</a>
+        <div class="right menu auth"><a href="#"><i class="icon-basket-icon"></i><span>Grozs</span></a></div>
+      </nav>
+    </div>
+  </div>
+</header>
