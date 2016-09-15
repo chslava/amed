e5c78018f4d28e5ca644b5bf032f57f8d1d99fb2
Aivars Lauzis <lauzis@inbox.lv>
2016-09-15 17:54:52 +0300

    lates posts code added

--- wp-content/themes/amedical/includes/parts/homepage_latest_posts.php
+++ wp-content/themes/amedical/includes/parts/homepage_latest_posts.php
@@ -1,7 +1,57 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lauzis
- * Date: 16.15.9
- * Time: 17:13
- */
+<section class="home-news very padded">
+    <div class="ui container">
+        <h2 class="ui centered header centered decored"><?php _e('News / Blog','amedical'); ?></h2>
+        <div class="ui grid">
+
+
+            <?php
+            // the query
+            $args =array();
+            $args['ļimit']=2;
+            $the_query = new WP_Query( $args ); ?>
+
+            <?php if ( $the_query->have_posts() ) : ?>
+
+                <!-- pagination here -->
+
+                <!-- the loop -->
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+
+                    <div class="sixteen wide mobile eight wide computer column">
+                        <div class="image-container">
+                            <?php the_post_thumbnail("homepage-blog-image"); ?>
+                            <time>
+                                <span><?php the_date("d"); ?></span>
+                                <?php the_date("M"); ?></time>
+                        </div>
+                        <h3 class="ui header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <p>
+                            <?php the_excerpt(); ?>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php _e("Read more",'amedical'); ?><i class="long right arrow icon"></i>
+                            </a>
+                        </p>
+                    </div>
+                <?php endwhile; ?>
+                <!-- end of the loop -->
+
+                <!-- pagination here -->
+
+                <?php wp_reset_postdata(); ?>
+
+            <?php else : ?>
+                <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <?php endif; ?>
+
+
+
+
+        </div>
+        <div class="aligned center view-all">
+            <a class="ui primary button" href="<?php the_field("link_to_all_news_page","option"); ?>"><?php _e("View all articles",'amedical'); ?>
+                <i class="long right arrow icon"></i>
+            </a>
+        </div>
+    </div>
+</section>
