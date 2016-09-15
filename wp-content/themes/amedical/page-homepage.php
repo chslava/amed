3890bc4daba3ef5430ac616cab6957f115f8544b
Aivars Lauzis <lauzis@inbox.lv>
2016-09-15 17:01:55 +0300

    added home page parts

--- wp-content/themes/amedical/page-homepage.php
+++ wp-content/themes/amedical/page-homepage.php
@@ -5,5 +5,11 @@ Template Name: Homepage
   get_header();
 ?>
 <?php include("includes/parts/homepage_slider.php"); ?>
-  
-  <?php get_footer(); ?>
+
+<?php include("includes/parts/homepage_services.php"); ?>
+
+<?php include("includes/parts/homepage_benefits.php"); ?>
+
+<?php include("includes/parts/homepage_we_represent.php"); ?>
+
+<?php get_footer(); ?>
