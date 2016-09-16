201e8da0e6d581035c2ec3222cff4d9ed1e141ba
Ronald Wilson <ronaldwilson2005@gmail.com>
2016-09-16 14:26:17 +0300

    Breadcrumbs

--- wp-content/themes/amedical/single.php
+++ wp-content/themes/amedical/single.php
@@ -5,10 +5,7 @@ get_header();
 ?>
 
 <main class="ui container page">
-  <div class="ui breadcrumb"><a class="section" href="index.html">Sākums</a>
-    <div class="divider">//</div>
-    <div class="active section">Content</div>
-  </div>
+  <?php include_once('includes/parts/breadcrumbs.php');?>
   <div class="ui grid">
 			<?php while ( have_posts() ) : the_post(); ?>
           <h1> <?php the_title(); ?></h1>
