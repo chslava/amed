069f156ef4a9f2d9b99ff785249d7cad9b6f2549
Aivars Lauzis <lauzis@inbox.lv>
2016-09-16 15:56:39 +0300

    footer code update

--- wp-content/themes/amedical/footer.php
+++ wp-content/themes/amedical/footer.php
@@ -2,9 +2,9 @@
   <div class="ui container">
     <form class="ui form">
       <div class="ui centered relaxed grid">
-        <div class="title">Pieteikties Jaunumiem</div>
-        <input class="subscribe ui input" placeholder="Ievadi savu e-pastu">
-        <button class="ui primary button">Pieteikties</button>
+        <div class="title"><?php the_field("newsletter_title","option"); ?></div>
+        <input class="subscribe ui input" placeholder="<?php the_field("newsletter_placeholder","option"); ?>">
+        <button class="ui primary button"><?php the_field("newsletter_cta_button","option"); ?></button>
       </div>
     </form>
   </div>
@@ -16,7 +16,6 @@
         <nav class="ui text menu">
           <div>
             <?php footer_menu(); ?>
-            <!-- <a class="item active" href="#"> Medicīnas iekārtas<a class="item" href="#2">Medicīnas preces</a><a class="item" href="#3">Industriālie un drošības risinājumi</a><a class="item" href="#4">Projektēšana</a><a class="item" href="#5">Serviss</a><a class="item" href="#6">Piegāde</a><a class="item" href="#7">Atgriešana un apmaiņa</a><a class="item" href="#8">Privātuma politika</a><a class="item" href="#9">Par mums</a><a class="item" href="contact-us.html">Kontakti</a></a> -->
           </div>
         </nav>
       </div>
@@ -24,19 +23,54 @@
         <div class="ui grid">
           <div class="sixteen wide mobile eight wide tablet ten wide computer column footer-contacts">
             <div class="ui vertical text menu">
-              <div class="title">Kontakti</div>
-              <div class="item"><i class="c-icon icon-place-icon"></i>Adrese: Varkaļu iela 13A, RĪga, Latvija, LV-1067</div>
-              <div class="item"><i class="c-icon icon-phone-icon"></i>Tālrunis: <a href="tel:66103002">66103002, </a> <a href="tel:20277153">20277153 </a></div>
-              <div class="item"><i class="c-icon icon-mail-icon"></i>E-pasts: <a href="mailto:info@amedical.eu">info@amedical.eu</a></div>
+              <div class="title"><?php the_field('contacts_title','option'); ?></div>
+              <div class="item">
+                <i class="c-icon icon-place-icon"></i>
+                <?php the_field("address","option"); ?>
+              </div>
+              <div class="item">
+                <i class="c-icon icon-phone-icon"></i>
+                <?php the_field('phone_title','option'); ?>:
+                <a href="tel:<?php the_field("phone","option"); ?>">
+                  <?php the_field("phone","option"); ?>
+                </a>
+                <?php if (get_field("phone-2","option")): ?>,
+                  <a href="tel:<?php the_field("phone-2","option"); ?>">
+                    <?php the_field("phone-2","option"); ?>
+                  </a>
+                <?php endif; ?>
+              </div>
+
+              <div class="item">
+                <i class="c-icon icon-mail-icon"></i>
+                <?php the_field('email_title','option'); ?>:
+                <a href="mailto:<?php the_field("email","option"); ?>">
+                  <?php the_field("email","option"); ?>
+                </a>
+              </div>
             </div>
           </div>
           <div class="sixteen wide mobile eight wide tablet six wide computer column follow-us">
-            <div class="title">Seko mums</div>
-            <div class="ui text menu soc-menu"><a class="item"><i class="soc-icon icon-facebook-icon"></i>Facebook</a><a class="item"><i class="soc-icon icon-linkedin-icon"></i>LinkedIn</a></div>
+            <div class="title"><?php the_field('social_title','option'); ?></div>
+            <div class="ui text menu soc-menu">
+
+              <?php if (get_field("facebook","option")): ?>
+                <a class="item" target="_blank" href="<?php the_field("facebook","option"); ?>">
+                  <i class="soc-icon icon-facebook-icon"></i>
+                </a>
+              <?php endif; ?>
+
+              <?php if (get_field("linkedin","option")): ?>
+                <a class="item" target="_blank" href="<?php the_field("linkedin","option"); ?>">
+                  <i class="soc-icon icon-linkedin-icon"></i>
+                </a>
+              <?php endif; ?>
+
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="copyright">Visas tiesības aizsargātas &copy 2016 A.MEDICAL</div>
+  <div class="copyright"><?php the_field("copyright_text","option"); ?></div>
 </footer>
