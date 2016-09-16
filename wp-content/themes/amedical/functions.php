3d843ceb3b901997496fb4936b84425f775760d2
Aivars Lauzis <lauzis@inbox.lv>
2016-09-16 14:39:50 +0300

    updated read more ending

--- wp-content/themes/amedical/functions.php
+++ wp-content/themes/amedical/functions.php
@@ -31,3 +31,9 @@
 
  // Add post types
   //require_once('includes/post_types/slider.php');
+
+
+function wpdocs_excerpt_more( $more ) {
+ return '...';
+}
+add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
