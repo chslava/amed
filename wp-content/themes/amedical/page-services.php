12d8d5258d3a76ea2d7f43c9e8ff30ce910fb5d8
Aivars Lauzis <lauzis@inbox.lv>
2016-09-20 10:37:35 +0300

    removed layout legacy

---
+++ wp-content/themes/amedical/page-services.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Services
+*/
+get_header();
+
+?>
+
+<main class="ui container page">
+    <?php include_once('includes/parts/breadcrumbs.php');?>
+    <div class="ui grid">
+        <?php while ( have_posts() ) : the_post(); ?>
+            <h1> <?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        <?php endwhile;?>
+    </div>
+</main>
+
+<?php get_footer(); ?>
