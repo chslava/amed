c4d918a3ca363abca161ffd2110b9c0e9ede371c
Aivars Lauzis <lauzis@inbox.lv>
2016-09-20 18:11:23 +0300

    mobile search layout added

--- wp-content/themes/amedical/includes/parts/mobile_logo_search.php
+++ wp-content/themes/amedical/includes/parts/mobile_logo_search.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lauzis
- * Date: 16.20.9
- * Time: 17:58
- */
+<div class="ui container grid mobile tablet only">
+    <div class="two column row">
+        <div class="left aligned column">
+            <a class="logo" href="<?php home_url(); ?>">
+                <img src="<?php echo get_stylesheet_directory_uri() ;?>/img/logo.png" alt="<?php print(get_bloginfo( 'name' )); ?>" title="<?php print(get_bloginfo( 'name' )); ?>">
+            </a>
+        </div>
+        <div class="right aligned column">
+            <a class="mob-a mob-toogle-menu" href="#">
+                <i class="icon-mob-toogle-open"></i>
+            </a>
+            <a class="mob-a mob-search" href="#">
+                <i class="search icon"></i>
+            </a>
+        </div>
+    </div>
+    <div class="one column row search-row" style="display:none">
+        <div class="column">
+            <form class="ui large form" action="<?php the_field("search_url","option"); ?>" method="GET">
+                <div class="field">
+                    <input placeholder="<?php _e("Search",'amedical'); ?>">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
