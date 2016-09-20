70bdb093e01211786ea564b79ddfd0ee1c6e9a5d
Aivars Lauzis <lauzis@inbox.lv>
2016-09-20 10:39:55 +0300

    page layouts added

---
+++ wp-content/themes/amedical/page-projects.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Template Name: Projects
+*/
+get_header();
+
+?>
+<?php while ( have_posts() ) : the_post(); ?>
+    <main class="ui container projects page">
+        <?php include_once('includes/parts/breadcrumbs.php');?>
+        <div class="ui grid">
+            <div class="ui row">
+                <div class="sixteen wide mobile eight wide tablet eight wide computer column">
+                    <h1 class="ui left header decored"><?php the_title(); ?></h1>
+                    <?php the_content(); ?>
+                </div>
+                <div class="sixteen wide mobile eight wide tablet eight wide computer column">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php $counter=0; ?>
+    <?php while(have_rows("designing_content")): the_row(); ?>
+        <section class="secondary-services">
+            <div class="ui container">
+
+                <h2 class="ui header centered decored">
+                  <?php the_sub_field("title"); ?>
+                </h2>
+
+                <div class="ui grid">
+                    <div class="center aligned ten wide centered column">
+                       <p><?php the_sub_field("content"); ?></p>
+                    </div>
+                    <?php $counter=0; ?>
+                    <?php while(have_rows("images")): the_row(); ?>
+
+                        <?php if ($counter%3==0): ?>
+                            <div class="three column row">
+                        <?php endif; ?>
+
+                        <?php $image = get_sub_field("image"); ?>
+
+                        <img class="ui image column" src="<?php print($image['sizes']['services-images']); ?>" />
+
+                        <?php if ($counter%3==2): ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php $counter++; ?>
+                    <?php endwhile; ?>
+
+                    <?php if ($counter%3<2 && $counter>0): ?>
+                        </div>
+                    <?php endif; ?>
+
+            </div>
+            </div>
+        </section>
+
+    <?php endwhile; ?>
+
+
+
+    <?php if (get_field("cta_block_title")): ?>
+        <section class="ui container very padded">
+            <div class="ui grid">
+                <div class="ui stretched row">
+                    <div class="sixteen wide mobile eight wide tablet ten wide computer column">
+                        <h2 class="ui left header decored">
+                            <?php the_field("cta_block_title"); ?>
+                        </h2>
+                        <?php the_field("cta_block_text"); ?>
+                    </div>
+                    <div class="sixteen wide mobile eight wide tablet six wide computer column">
+                        <div class="ui raised center aligned padded segment">
+                            <h3 class="title padded large">
+                                <?php the_field("cta_block_tagline"); ?>
+                            </h3>
+                            <p>
+                                <a class="ui basic fluid button" href="<?php the_field("cta_block_link"); ?>">
+                                    <?php the_field("cta_block_button"); ?>
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php endif; ?>
+
+
+<?php endwhile;?>
+
+
+
+<?php get_footer(); ?>
--- wp-content/themes/amedical/page-services.php
+++ wp-content/themes/amedical/page-services.php
@@ -5,15 +5,53 @@ Template Name: Services
 get_header();
 
 ?>
+<?php while ( have_posts() ) : the_post(); ?>
+    <main class="ui container projects page">
+        <?php include_once('includes/parts/breadcrumbs.php');?>
+            <div class="ui grid">
+                <div class="ui row">
+                    <div class="sixteen wide mobile eight wide tablet eight wide computer column">
+                        <h1 class="ui left header decored"><?php the_title(); ?></h1>
+                        <?Php the_content(); ?>
+                    </div>
+                    <div class="sixteen wide mobile eight wide tablet eight wide computer column">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                </div>
+                <div class="stretched very padded row">
 
-<main class="ui container page">
-    <?php include_once('includes/parts/breadcrumbs.php');?>
-    <div class="ui grid">
-        <?php while ( have_posts() ) : the_post(); ?>
-            <h1> <?php the_title(); ?></h1>
-            <?php the_content(); ?>
-        <?php endwhile;?>
-    </div>
-</main>
+                    <div class="sixteen wide mobile eight wide tablet eleven wide computer column">
+
+                        <h2 class="ui left header decored"><?php the_field("service_title"); ?></h2>
+                        <?php the_field("service_content"); ?>
+
+                        <div class="ui basic compact segment">
+                            <h3 class="large"><?php the_field("service_cta_text"); ?></h3>
+                            <p>
+                                <a class="ui basic fluid button" href="<?php the_field("service_cta_button_link"); ?>">
+                                    <?php the_field("service_cta_button_text"); ?>
+                                </a>
+                            </p>
+                        </div>
+
+                    </div>
+                    <div class="sixteen wide mobile eight wide tablet five wide computer column">
+                        <div class="ui raised center aligned padded segment">
+                            <div class="benefits-icon icon-service-phone-icon"></div>
+                            <div class="large title"><?php the_field("service_phone_title"); ?></div>
+                            <div class="large title"><?php the_field("service_phone"); ?></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </main>
+
+
+    </main>
+
+<?php endwhile;?>
+
+
 
 <?php get_footer(); ?>
