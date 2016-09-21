2ad94fce8f07b8372b14f0c804d849c43864d79c
Aivars Lauzis <lauzis@inbox.lv>
2016-09-21 18:35:43 +0300

    bradcrumbs style overwritten

--- wp-content/themes/amedical/includes/parts/breadcrumbs.php
+++ wp-content/themes/amedical/includes/parts/breadcrumbs.php
@@ -1,5 +1,16 @@
-
+<div class="ui breadcrumb">
   <?php if ( function_exists('yoast_breadcrumb') ){
-          yoast_breadcrumb('<div class="ui breadcrumb">','</div>');
+          $breadcrumbs = yoast_breadcrumb('','',false);
+      $breadcrumbs = strip_tags($breadcrumbs,"<a>");
+      $breadcrumbs = str_replace('<a ','<a class="section"',$breadcrumbs);
+      $breadcrumbs = str_replace('http://','[http]',$breadcrumbs);
+      $breadcrumbs = str_replace('https://','[https]',$breadcrumbs);
+      $breadcrumbs = str_replace('//','<div class="divider">//</div><div class="active section">&nbsp;',$breadcrumbs);
+      $breadcrumbs = str_replace('[https]','https://',$breadcrumbs);
+      $breadcrumbs = str_replace('[http]','http://',$breadcrumbs);
+      $breadcrumbs.= '</div>';
+
+            print($breadcrumbs);
        }
   ?>
+</div>
