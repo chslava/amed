2e0b3ec3b3b9419193e31b0c3a503cbe7c56a51b
Aivars Lauzis <lauzis@inbox.lv>
2016-09-22 02:15:28 +0300

    breadcrumbs update, fixed some unclosed div of there is third leve item

--- wp-content/themes/amedical/includes/parts/breadcrumbs.php
+++ wp-content/themes/amedical/includes/parts/breadcrumbs.php
@@ -5,10 +5,13 @@
       $breadcrumbs = str_replace('<a ','<a class="section"',$breadcrumbs);
       $breadcrumbs = str_replace('http://','[http]',$breadcrumbs);
       $breadcrumbs = str_replace('https://','[https]',$breadcrumbs);
-      $breadcrumbs = str_replace('//','<div class="divider">//</div><div class="active section">&nbsp;',$breadcrumbs);
+      $breadcrumbs = str_replace('//','<div class="divider">//</div>',$breadcrumbs);
       $breadcrumbs = str_replace('[https]','https://',$breadcrumbs);
       $breadcrumbs = str_replace('[http]','http://',$breadcrumbs);
-      $breadcrumbs.= '</div>';
+      $last = explode("</div>",$breadcrumbs);
+      $last = end($last);
+      $breadcrumbs = str_replace($last,'<div class="active section">'.$last."</div>",$breadcrumbs);
+      
 
             print($breadcrumbs);
        }
