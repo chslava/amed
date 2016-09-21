529bdf3fe2a9ad89e08671d9b52dadaa43197b55
Aivars Lauzis <lauzis@inbox.lv>
2016-09-21 23:09:33 +0300

    benefits layout fixed

--- wp-content/themes/amedical/includes/parts/homepage_benefits.php
+++ wp-content/themes/amedical/includes/parts/homepage_benefits.php
@@ -2,18 +2,18 @@
 <?php $benefit_text = get_field("benefit_text"); ?>
 
 <?php if (have_rows("benefit_items")): ?>
-    <section class="benefits ui container very padded">
-        <div class="ui relaxed grid">
+    <section class="benefits ui container">
+        <div class="ui grid">
             <div class="sixteen wide column">
                 <h2 class="ui header centered decored"><?php print($benefit_title); ?></h2>
                 <p class="content">
-                    <?php print($benefit_text); ?>
+                    <?php print(strip_tags($benefit_text)); ?>
                 </p>
             </div>
             <div class="ui stretched row">
                 <?php while(have_rows("benefit_items")): ?>
                     <?php the_row(); ?>
-                    <div class="fourteen wide centered mobile eight wide tablet four wide computer column">
+                    <div class="sixteen wide centered mobile eight wide tablet four wide computer column">
                         <div class="ui raised segment">
                             <div class="benefits-icon <?php the_sub_field("benefit_item_icon"); ?>"></div><strong><?php the_sub_field("benefit_item_title"); ?></strong>
                             <p><?php the_sub_field("benefit_item_text"); ?></p>
@@ -25,3 +25,5 @@
         </div>
     </section>
 <?php endif; ?>
+
+
