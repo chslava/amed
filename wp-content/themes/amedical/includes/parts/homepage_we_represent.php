711b65a42941db7d60cc2db3f0ee2e21b80aaace
Aivars Lauzis <lauzis@inbox.lv>
2016-09-21 12:20:06 +0300

    home parters updated fixed

--- wp-content/themes/amedical/includes/parts/homepage_we_represent.php
+++ wp-content/themes/amedical/includes/parts/homepage_we_represent.php
@@ -5,13 +5,12 @@
     </h2>
     <div class="ui six column center aligned grid">
         <ul class="represent-slider">
-            <?php if (have_rows("we_represent_bramd_items")): ?>
+
                 <?php
                 $represent_params = array(
                     'post_type' => 'partner'
                 );
                 $represent_q= new WP_Query( $represent_params );
-
                 if ( $represent_q->have_posts() ) {
                     // The 2nd Loop
                     while ( $represent_q->have_posts() ) :
@@ -31,11 +30,6 @@
                     wp_reset_postdata();
                 }
                 ?>
-                <?php while(have_rows("we_represent_bramd_items")): ?>
-                    <?php the_row(); ?>
-
-                <?php endwhile; ?>
-            <?php endif; ?>
         </ul>
 <!--        <div class="center aligned row">-->
 <!--            <a class="ui primary button" href="--><?php //the_field("we_represent_link","option"); ?><!--">--><?php //the_field("we_represent_cta","option"); ?>
