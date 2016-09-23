81c177efa2a3dfbc79613f0cae1bff9c559adfaf
Aivars Lauzis <lauzis@inbox.lv>
2016-09-23 17:33:45 +0300

    some contact page updates missed from front end

--- wp-content/themes/amedical/page-contacts.php
+++ wp-content/themes/amedical/page-contacts.php
@@ -8,44 +8,39 @@ get_header();
 
 <main class="contacts">
   <div class="ui container">
-    <div class="sixteen wide mobile eight wide tablet five wide computer column">
-      <?php include_once('includes/parts/breadcrumbs.php'); ?>
-      <h1 class="ui header"><?php echo get_the_title(); ?></h1>
-      <p><?php echo the_field('address_title','option'); ?>: <?php echo the_field('address','option'); ?></p>
-      <p><?php echo the_field('phone_title','option'); ?>:
-          <a href="tel:<?php echo the_field('phone','option'); ?>">
-            <?php echo the_field('phone','option'); ?>
-          </a>,
-          <a href="tel:<?php echo the_field('phone-2','option'); ?>">
-              <?php echo the_field('phone-2','option'); ?>
-          </a>
-      </p>
-      <p><?php echo the_field('fax_title','option'); ?>: <?php echo the_field('fax','option'); ?></p>
-      <p><?php echo the_field('email_title','option'); ?>: <a href="mailto:<?php echo the_field('email','option'); ?>"><?php echo the_field('email','option'); ?></a></p>
-      <p><?php echo the_field('opening_hours_title','option'); ?>: <?php echo the_field('opening_hours','option'); ?></p>
-      <div class="ui divider"></div>
-      <h2 class="ui header"><?php echo the_field('title_registration','option'); ?></h2>
-      <p><?php echo the_field('registration_name','option'); ?></p>
-      <p><?php echo the_field('registration_no','option'); ?></p>
-      <p><?php echo the_field('registration_address','option'); ?></p><br>
-      <p><?php echo the_field('bank_name','option'); ?></p>
-      <p><?php echo the_field('bank_account','option'); ?></p>
-      <p><?php echo the_field('bank_code','option'); ?></p>
+      <div class="ui grid">
+          <div class="sixteen wide mobile eight wide tablet five wide computer column">
+
+              <?php include_once('includes/parts/breadcrumbs.php'); ?>
+
+              <h1 class="ui header"><?php echo get_the_title(); ?></h1>
+              <p><?php echo the_field('address_title','option'); ?>: <?php echo the_field('address','option'); ?></p>
+              <p><?php echo the_field('phone_title','option'); ?>:
+                  <a href="tel:<?php echo the_field('phone','option'); ?>">
+                    <?php echo the_field('phone','option'); ?>
+                  </a>,
+                  <a href="tel:<?php echo the_field('phone-2','option'); ?>">
+                      <?php echo the_field('phone-2','option'); ?>
+                  </a>
+              </p>
+              <p><?php echo the_field('fax_title','option'); ?>: <?php echo the_field('fax','option'); ?></p>
+              <p><?php echo the_field('email_title','option'); ?>: <a href="mailto:<?php echo the_field('email','option'); ?>"><?php echo the_field('email','option'); ?></a></p>
+              <p><?php echo the_field('opening_hours_title','option'); ?>: <?php echo the_field('opening_hours','option'); ?></p>
+              <div class="ui divider"></div>
+              <h2 class="ui header"><?php echo the_field('title_registration','option'); ?></h2>
+              <p><?php echo the_field('registration_name','option'); ?></p>
+              <p><?php echo the_field('registration_no','option'); ?></p>
+              <p><?php echo the_field('registration_address','option'); ?></p><br>
+              <p><?php echo the_field('bank_name','option'); ?></p>
+              <p><?php echo the_field('bank_account','option'); ?></p>
+              <p><?php echo the_field('bank_code','option'); ?></p>
+        </div>
     </div>
   </div>
   <div class="info"></div>
 
-  <?php
-        $ArrMap = the_field('map_location','option');
-        //echo "<pre> *****"; print_r($ArrMap);exit;
-        $lat = $ArrMap['lat'] > 0 ? $ArrMap['lat'] : '56.9638746';
-        $lng = $ArrMap['lng'] > 0 ? $ArrMap['lng'] : '24.0318992';
-        //$lat = '56.9638746';
-        //$lng = '24.0318992';
+  <div class="map contact-map" data-lat="<?php echo get_field("lat","option"); ?>" data-lng="<?php echo get_field("lng","option"); ?>"></div>
 
-  ?>
-
-  <div class="map contact-map" data-lat="<?php echo $lat; ?>" data-lng="<?php echo $lng; ?>"></div>
 
   <script>
     jQuery(function($) {
