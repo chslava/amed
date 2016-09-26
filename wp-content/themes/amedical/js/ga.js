f2169574541d5fded2bf76d593a998376c313635
Aivars Lauzis <lauzis@inbox.lv>
2016-09-26 23:44:50 +0300

    google analytics event script

--- wp-content/themes/amedical/js/ga.js
+++ wp-content/themes/amedical/js/ga.js
@@ -1,5 +1,17 @@
-jQuery(document).ready(
-    function($){
+function remove_params_from_url(oldURL) {
+    var index = 0;
+    var newURL = oldURL;
+    index = oldURL.indexOf('?');
+    if(index == -1){
+        index = oldURL.indexOf('#');
+    }
+    if(index != -1){
+        newURL = oldURL.substring(0, index);
+    }
+    return newURL;
+}
+
+jQuery(function($) {
 
 
         //lang variable defined in head part of the output html
@@ -16,11 +28,15 @@ jQuery(document).ready(
 
 
         //2.. Search bar - add event for using the search bar
-        $( "#search-mobile-form, #search-form" ).submit(function( event ) {
+        $('#search-mobile-form, #search-form').submit(function( event ) {
             var self = $(this);
-            var searchterm = self.find(input[name="search"]).val();
-            ga('send', 'event', 'Search bar used', searchterm);
+            var searchterm = self.find('input[name=search]').first().val();
+            ga('send', 'event', 'Search bar used', searchterm.trim());
         });
+
+
+
+
 
 
         //3.. Contact form - add event in the contacts page for successfully sending the contact form
@@ -42,11 +58,12 @@ jQuery(document).ready(
             var self = $(this);
             var selected_email = $(this).text();
             var click_position="Footer email clicked";
+
             //check if link is in footer block if it not in footer, must be in contacts
-            if (!self.closest("foooter")){
+            if (self.parents("footer").length==0){
                 click_position='Contacts email clicked';
             }
-            ga('send', 'event', 'Email address clicked', click_position, selected_email );
+            ga('send', 'event', 'Email address clicked', click_position, selected_email.trim() );
         })
 
 
@@ -56,18 +73,20 @@ jQuery(document).ready(
         $('a[href^="tel:"]').click(function(){
             //4.a 4.b
             var self = $(this);
-            var selected_email = $(this).text();
-            var click_position="Footer email clicked";
+            var selected_phone = $(this).text();
+            var click_position="Footer phone clicked";
+
             //check if link is in footer block if it not in footer, must be in contacts
-            if (!self.closest("footer")){
-                click_position='Contacts email clicked';
+            if (self.parents("footer").length==0){
+                click_position='Contacts phone clicked';
             }
-            ga('send', 'event', 'Phone number clicked', click_position, selected_email );
+
+            ga('send', 'event', 'Phone number clicked', click_position, selected_phone.trim());
         })
 
 
         //6.. Newsletter - add event for successfully subscribing to the newsletter
-        if(('.mc4wp-success').length>0){
+        if($('.mc4wp-success').length>0){
             ga('send', 'event', 'Newsletter subscribed', lang);
         }
 
@@ -75,11 +94,11 @@ jQuery(document).ready(
         //7.. Homepage events - add events for clicking various homepage elements
         //a.. Slider button - add event for clicking the slider button
 
-        $('main-slider').find('a').click(function{
+        $('main-slider').find('a').click(function(){
             var self=$(this);
             var button_text = self.text();
             var button_url = self.attr("href");
-            ga('send', 'event', 'Homepage events', button_text+' button clicked', button_url);
+            ga('send', 'event', 'Homepage events', button_text.trim()+' button clicked', button_url);
         });
 
         //b.. Uzzināt vairāk buttons - add event for clicking one of the “Uzzināt vairāk” buttons (for all languages)
@@ -87,17 +106,17 @@ jQuery(document).ready(
             var self=$(this);
             var button_text = self.text();
             var button_url = self.attr("href");
-            ga('send', 'event', 'Homepage events', button_text+' button clicked', button_url);
+            ga('send', 'event', 'Homepage events', button_text.trim()+' button clicked', button_url);
         });
 
         //c.. Mēs pārstāvam button - add event for clicking the Mēs pārstāvam content block button (for all languages)
         //d.. Apsaktīt visus jaunumus button - add event for clicking the news/blog content block button (for all languages)
-        $('#we-represent-button, #all-news').function(){
+        $('#we-represent-button, #all-news').click(function(){
             var self=$(this);
             var button_text = self.text();
             var button_url = self.attr("href");
-            ga('send', 'event', 'Homepage events', button_text+' button clicked', button_url);
-        };
+            ga('send', 'event', 'Homepage events', button_text.trim()+' button clicked', button_url);
+        });
 
 
 
@@ -105,18 +124,24 @@ jQuery(document).ready(
         //done in php
 
         //8.1.. Contact us buttons - add event for clicking the “Contact us” button (for all languages)
-        $('a.button').each(function(){
+        $('a').each(function(){
             var self = $(this);
+            if(self.hasClass("button")){
 
-            var url="";
-            if (self.attr("href")){
-                url =self.attr("href");
-                text = self.text();
-                if (url == contact_page_link){
-                    ga('send', 'event', 'Contact us button clicked', text+' button clicked');
+                var url="";
+                url = self.attr("href");
+                if (url.length>0){
+                    text = self.text();
+                    if (remove_params_from_url(url) == contact_page_link){
+                        self.click(function(){
+                            ga('send', 'event', 'Contact us button clicked', text.trim()+' button clicked');
+                        });
+
+                    }
+
                 }
-
             }
+
         });
 
 
