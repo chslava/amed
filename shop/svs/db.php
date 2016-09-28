5dc4d2aa25887f4c97bae20c51b4a6ddee4adc0d
alex <netsh91@gmail.com>
2016-09-28 13:12:23 +0300

    fixing /shop/svs configuration

--- shop/svs/db.php
+++ shop/svs/db.php
@@ -1,21 +1,8 @@
 <?php
 $url = 'http://';
 
-if(!getenv('APPLICATION_ENV')){define('APPLICATION_ENV','production');}else{define('APPLICATION_ENV',getenv('APPLICATION_ENV'));}
-if(APPLICATION_ENV == 'development'){
-	$url .= $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
-	$db_host="localhost"; //MySql hostname
-	$db_user="amedical_adm";//MySql username
-	$db_password="raweicieya0ai9yo";//MySql password
-	$db_name="amedical";//MySql database name
-}
-else{
-	$url .= str_replace('\\','',$_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']));
-	$db_host = "sql-05.deac.lv";
-	$db_user="amedical_adm";//MySql username
-	$db_password="raweicieya0ai9yo";//MySql password
-	$db_name="amedical";//MySql database name
-}
+require_once (dirname(dirname(__FILE__)) . "/config.php");
+
 $result_db = mysqli_connect($db_host,$db_user,$db_password);
 mysqli_select_db($result_db, $db_name);
 
