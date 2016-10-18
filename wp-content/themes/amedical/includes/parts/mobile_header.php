0d23734340a79716815ba884de1de07686412412
Aivars Lauzis <lauzis@inbox.lv>
2016-10-18 11:50:14 +0300

    separated between files the mobile, header, destop

---
+++ wp-content/themes/amedical/includes/parts/mobile_header.php
@@ -0,0 +1,24 @@
+<div class="two column row">
+
+    <div class="left aligned column">
+        <a href="<?php echo home_url('/'); ?>" class="logo">
+            <img src="<?php echo get_stylesheet_directory_uri() ;?>/img/logo.png" alt="<?php print(get_bloginfo( 'name' )); ?>" title="<?php print(get_bloginfo( 'name' )); ?>">
+        </a>
+    </div>
+
+    <div class="right aligned column">
+        <a class="mob-toogle-menu" href="#">
+            <i class="icon-mob-toogle-open"></i>
+        </a><a class="mob-search" href="#"><i class="search icon"></i></a></div>
+</div>
+<div class="one column row search-row" style="display:none">
+    <div class="column">
+        <form id="search-mobile-form" class="ui large form" action="<?php the_field("search_url","option"); ?>" method="GET">
+            <div class="field">
+                <input placeholder="Meklēt...">
+            </div>
+        </form>
+    </div>
+</div>
+
+
---
+++ wp-content/themes/amedical/includes/parts/tablet_header.php
@@ -0,0 +1,18 @@
+<div class="two column row">
+
+    <div class="left aligned column">
+        <a href="<?php echo home_url('/'); ?>" class="logo">
+            <img src="<?php echo get_stylesheet_directory_uri() ;?>/img/logo.png" alt="<?php print(get_bloginfo( 'name' )); ?>" title="<?php print(get_bloginfo( 'name' )); ?>">
+        </a>
+    </div>
+
+    <div class="right aligned column">
+        <div class="item computer-search">
+            <form id="search-tablet-form" action="<?php the_field("search_url","option"); ?>" method="GET">
+                <div class="ui icon input search">
+                    <input placeholder="Meklēt"><i class="search icon"></i>
+                </div>
+            </form>
+        </div><a class="mob-toogle-menu" href="#"><i class="icon-mob-toogle-open"></i></a>
+    </div>
+</div>
