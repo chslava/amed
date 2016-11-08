c9df6664a0a73054694061f3682d912c6ebe04e0
Aivars Lauzis <lauzis@inbox.lv>
2016-11-08 16:08:18 +0200

    plugins updated

--- wp-content/plugins/above-the-fold-optimization/public/js/src/abovethefold.loadcss-modified.js
+++ wp-content/plugins/above-the-fold-optimization/public/js/src/abovethefold.loadcss-modified.js
@@ -6,150 +6,158 @@
  *
  * @package    abovethefold
  * @subpackage abovethefold/public
- * @author     Optimalisatie.nl <info@optimalisatie.nl>
+ * @author     PageSpeed.pro <info@pagespeed.pro>
  */
 
-window['abovethefold'].loadCSS = function( href, before, media, callback ) {
+(function(window, Abtf) {
 
-    if (window['abovethefold'].debug) {
-        console.info('abovethefold.css() > loadCSS()[RAF] async download start', href);
-    }
+    window['Abtf'].loadCSS = function( href, before, media, callback ) {
 
-    // Arguments explained:
-    // `href` [REQUIRED] is the URL for your CSS file.
-    // `before` [OPTIONAL] is the element the script should use as a reference for injecting our stylesheet <link> before
-        // By default, loadCSS attempts to inject the link after the last stylesheet or script in the DOM. However, you might desire a more specific location in your document.
-    // `media` [OPTIONAL] is the media type or query of the stylesheet. By default it will be 'all'
-    var doc = window.document;
-    var ss = doc.createElement( "link" );
-    var ref;
-    if( before ){
-        ref = before;
-    }
-    else {
-        var refs = ( doc.body || doc.getElementsByTagName( "head" )[ 0 ] ).childNodes;
-        ref = refs[ refs.length - 1];
-    }
-
-    var sheets = doc.styleSheets;
-    ss.rel = "stylesheet";
-    ss.href = href;
-    // temporarily set media to something inapplicable to ensure it'll fetch without blocking render
-    ss.media = "only x";
-
-    // wait until body is defined before injecting link. This ensures a non-blocking load in IE11.
-    function ready( cb ){
-        if( doc.body ){
-            return cb();
-        }
-        setTimeout(function(){
-            ready( cb );
-        });
-    }
-    // Inject link
-    // Note: the ternary preserves the existing behavior of "before" argument, but we could choose to change the argument to "after" in a later release and standardize on ref.nextSibling for all refs
-    // Note: `insertBefore` is used instead of `appendChild`, for safety re: http://www.paulirish.com/2011/surefire-dom-element-insertion/
-    ready( function(){
-        ref.parentNode.insertBefore( ss, ( before ? ref : ref.nextSibling ) );
-    });
-
-    /**
-     * CSS rendered flag
-     */
-    var CSSrendered = false;
-
-    // A method (exposed on return object for external use) that mimics onload by polling until document.styleSheets until it includes the new sheet.
-    var onloadcssdefined = function( cb ){
-
-        if (CSSrendered) {
-            return;
+        if (ABTFDEBUG) {
+            console.info('Abtf.css() > loadCSS()[RAF] async download start', href);
         }
 
-        var resolvedHref = ss.href;
-        var i = sheets.length;
-        while( i-- ){
-            if (CSSrendered) {
-                break;
-            }
-            if( sheets[ i ].href === resolvedHref ){
+        // Arguments explained:
+        // `href` [REQUIRED] is the URL for your CSS file.
+        // `before` [OPTIONAL] is the element the script should use as a reference for injecting our stylesheet <link> before
+            // By default, loadCSS attempts to inject the link after the last stylesheet or script in the DOM. However, you might desire a more specific location in your document.
+        // `media` [OPTIONAL] is the media type or query of the stylesheet. By default it will be 'all'
+        var doc = window.document;
+        var ss = doc.createElement( "link" );
+        var ref;
+        if( before ){
+            ref = before;
+        }
+        else {
+            var refs = ( doc.body || doc.getElementsByTagName( "head" )[ 0 ] ).childNodes;
+            ref = refs[ refs.length - 1];
+        }
+
+        var sheets = doc.styleSheets;
+        ss.rel = "stylesheet";
+        ss.href = href;
+        // temporarily set media to something inapplicable to ensure it'll fetch without blocking render
+        ss.media = "only x";
+
+        // wait until body is defined before injecting link. This ensures a non-blocking load in IE11.
+        function ready( cb ){
+            if( doc.body ){
                 return cb();
             }
+            setTimeout(function(){
+                ready( cb );
+            });
         }
-        setTimeout(function() {
-            onloadcssdefined( cb );
+        // Inject link
+        // Note: the ternary preserves the existing behavior of "before" argument, but we could choose to change the argument to "after" in a later release and standardize on ref.nextSibling for all refs
+        // Note: `insertBefore` is used instead of `appendChild`, for safety re: http://www.paulirish.com/2011/surefire-dom-element-insertion/
+        ready( function(){
+            ref.parentNode.insertBefore( ss, ( before ? ref : ref.nextSibling ) );
         });
-    };
 
-    /**
-     * Render CSS when file is loaded
-     */
-    function renderCSS(){
+        /**
+         * CSS rendered flag
+         */
+        var CSSrendered = false;
 
-        // already rendered?
-        if (CSSrendered) {
-            return;
-        }
-        CSSrendered = true;
+        // A method (exposed on return object for external use) that mimics onload by polling until document.styleSheets until it includes the new sheet.
+        var onloadcssdefined = function( cb ){
 
-        if( ss.addEventListener ){
-            ss.removeEventListener( "load", renderCSS );
-        }
+            if (CSSrendered) {
+                return;
+            }
 
-        function render() {
+            var resolvedHref = ss.href;
+            var i = sheets.length;
+            while( i-- ){
+                if (CSSrendered) {
+                    break;
+                }
+                if( sheets[ i ].href === resolvedHref ){
+                    return cb();
+                }
+            }
+            setTimeout(function() {
+                onloadcssdefined( cb );
+            });
+        };
 
-            /**
-             * Use animation frame to paint CSS
-             *
-             * @link https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery
-             */
-            window['abovethefold'].raf(function() {
-                ss.media = media || "all";
-                if (window['abovethefold'].debug) {
-                    console.info('abovethefold.css() > loadCSS()[RAF] render', href);
+        /**
+         * Render CSS when file is loaded
+         */
+        function renderCSS(){
+
+            // already rendered?
+            if (CSSrendered) {
+                return;
+            }
+            CSSrendered = true;
+
+            if( ss.addEventListener ){
+                ss.removeEventListener( "load", renderCSS );
+            }
+
+            function render() {
+
+                /**
+                 * Use animation frame to paint CSS
+                 *
+                 * @link https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery
+                 */
+                Abtf.raf(function() {
+                    ss.media = media || "all";
+                    if (ABTFDEBUG) {
+                        console.info('Abtf.css() > loadCSS()[RAF] render', href);
+                    }
+
+                    /**
+                     * Callback on completion
+                     */
+                    if (callback) {
+                        callback();
+                    }
+                });
+            }
+
+            if (typeof Abtf.cnf.delay !== 'undefined' && parseInt(Abtf.cnf.delay) > 0) {
+
+                if (ABTFDEBUG) {
+                    console.info('Abtf.css() > loadCSS()[RAF] render delay', Abtf.cnf.delay, href);
                 }
 
                 /**
-                 * Callback on completion
+                 * Delayed rendering
                  */
-                if (callback) {
-                    callback();
-                }
-            });
+                setTimeout(render,Abtf.cnf.delay);
+            } else {
+                render();
+            }
         }
 
-        if (typeof window['abovethefold'].cnf.delay !== 'undefined' && parseInt(window['abovethefold'].cnf.delay) > 0) {
-
-            /**
-             * Delayed rendering
-             */
-            setTimeout(render,window['abovethefold'].cnf.delay);
+        // once loaded, set link's media back to `all` so that the stylesheet applies once it loads
+        if( ss.addEventListener ){
+            ss.addEventListener( "load", renderCSS);
         } else {
-            render();
+            ss.onload = renderCSS;
         }
-    }
 
-    // once loaded, set link's media back to `all` so that the stylesheet applies once it loads
-    if( ss.addEventListener ){
-        ss.addEventListener( "load", renderCSS);
-    } else {
-        ss.onload = renderCSS;
-    }
+        onloadcssdefined( renderCSS );
+        return ss;
 
-    onloadcssdefined( renderCSS );
-    return ss;
+    };
 
-};
+    window['Abtf'].raf = function(callback) {
+        if (typeof requestAnimationFrame === 'function') {
+            requestAnimationFrame(callback);
+        } else if (typeof mozRequestAnimationFrame === 'function') {
+            mozRequestAnimationFrame(callback);
+        } else if (typeof webkitRequestAnimationFrame === 'function') {
+            webkitRequestAnimationFrame(callback);
+        } else if (typeof msRequestAnimationFrame === 'function') {
+            msRequestAnimationFrame(callback);
+        } else {
+            Abtf.ready(callback);
+        }
+    };
 
-window['abovethefold'].raf = function(callback) {
-    if (typeof requestAnimationFrame === 'function') {
-        requestAnimationFrame(callback);
-    } else if (typeof mozRequestAnimationFrame === 'function') {
-        mozRequestAnimationFrame(callback);
-    } else if (typeof webkitRequestAnimationFrame === 'function') {
-        webkitRequestAnimationFrame(callback);
-    } else if (typeof msRequestAnimationFrame === 'function') {
-        msRequestAnimationFrame(callback);
-    } else {
-        window['abovethefold'].ready(callback);
-    }
-};
+})(window, window['Abtf']);
