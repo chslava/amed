471a778165e18d5ad8ecc5ab4fa91d965d5188a5
Aivars Lauzis <lauzis@inbox.lv>
2016-11-24 13:25:25 +0200

    added forbidden info page

---
+++ forbidden.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Access Forbidden</title>
+    <meta name="description" content="Acess forbidden">
+    <meta name="author" content="SitePoint">
+
+
+
+
+</head>
+
+<body>
+<h1>Access forbidden <?php print($_SERVER["remote_addr"]); ?></h1>
+<p>
+    Access restricted due to security reasons.
+</p>
+<p>
+    Please contact web developer or and server administration if You should have access to the website.
+</p>
+<img src="http://www.digitalscore.lv/wp-content/themes/ds-theme/images/logo.svg" />
+</body>
+</html>
