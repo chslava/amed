42847cc8620f55af32164d466884d53c4d2a3b73
Aivars Lauzis <lauzis@inbox.lv>
2016-11-30 16:13:45 +0200

    updated javascript in theme

--- wp-content/themes/amedical/js/main.js
+++ wp-content/themes/amedical/js/main.js
@@ -73,4 +73,12 @@ jQuery(function($) {
         $(this).children('i').toggleClass('icon-mob-toogle-close', 'icon-mob-toogle-open');
         $(this).children('i').toggleClass('icon-mob-toogle-open');
     })
+
+    $('.search.icon').on('click', function(e) {
+        e.preventDefault();
+        $('.input.search input').submit();
+        console.log("Form submitted");
+     })
+
+
 })
