4dfea58705d8ecffb40a2fe3a5e79a3b16a9ef0d
Eugene <kudriashov.e@gmail.com>
2017-04-13 11:33:12 +0300

    Sidebar (widgets area) template part

--- wp-content/themes/amedical/sidebar.php
+++ wp-content/themes/amedical/sidebar.php
@@ -1,3 +1,12 @@
-<aside class="sidebar">
-    <?php dynamic_sidebar('main-sidebar'); ?>
-</aside>
+<?php
+/**
+ * The sidebar(widgets area) template file for A.Medical Theme.
+ * @package amedical
+ * @author Eugene Kudriashov
+ */
+?>
+<div class="sixteen wide mobile six wide tablet five wide computer column" id="news-sidebar">
+	<div class="news-sidebar">
+		<?php dynamic_sidebar('main-sidebar'); ?>
+	</div>
+</div>
