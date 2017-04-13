b3433e3ea9c65e640a6a0c5acb3cf5d190c304c8
Eugene <kudriashov.e@gmail.com>
2017-04-13 11:32:37 +0300

    Main blog (news) template file

--- wp-content/themes/amedical/index.php
+++ wp-content/themes/amedical/index.php
@@ -1,37 +1,43 @@
-<?php get_header();
+<?php
+/**
+ * The main template file for A.Medical Theme.
+ * @package amedical
+ * @author Eugene Kudriashov
+ */
+get_header();
 ?>
+<main class="ui container news-page page">
+<?php get_template_part( 'includes/parts/breadcrumbs' ); ?>
+  <div class="ui grid">
+    <div class="ui row">
+      <?php if ( have_posts() ) : ?> 
+        <div class="sixteen wide mobile ten wide tablet eleven wide computer column" id="news-block-<?php the_ID(); ?>">   
+          <?php
+            while ( have_posts() ) : the_post();
+              $post_day = get_the_date( 'j' );
+              $post_month = get_the_date( 'M' );
+          ?>
+            <div class="news-item">
+              <div class="image-container">
+                <?php 
+                  if ( has_post_thumbnail() ) {
+                    the_post_thumbnail( 'news-blog-image', array('class' => 'ui shadowed image') );
+                  }
+                ?>
+                <time><span><?php echo $post_day; ?></span><?php echo $post_month; ?></time>
+              </div>
+              <?php the_title( '<h2 class="ui left header decored">', '</h2>' ); ?>
+              <?php the_excerpt(); ?>
+              <a href="<?php the_permalink(); ?>" class="ui basic button"><?php esc_html_e('Read more', 'dswp') ?></a>
+            </div>
+          <?php endwhile; ?>
 
-<main class="ui container page">
+          <?php dswp_amedical_pagination() ?>
+        </div>
+      <?php endif; ?>
 
-<?php include_once('includes/parts/breadcrumbs.php'); ?>
-
-<div class="row">
-	<div class="small-12 columns">
-		<section class="main-content" role="main">
-
-			<?php if ( have_posts() ) : ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
-					<h1><?php the_title(); ?></h1>
-					<?php the_content(); ?>
-				<?php endwhile; ?>
-
-			<?php endif;?>
-
-
-
-			<?php if ( is_paged() ) { ?>
-				<nav id="post-nav">
-					<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'dswp' ) ); ?></div>
-					<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'dswp' ) ); ?></div>
-				</nav>
-			<?php } ?>
-
-			</div>
-
-		</section>
-
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
 </main>
-		<?php // get_sidebar(); ?>
-</div>
 <?php get_footer(); ?>
