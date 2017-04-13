aa23c5dfe2bef0a7bde975f2fe1f6994ea608a65
Eugene <kudriashov.e@gmail.com>
2017-04-13 19:55:36 +0300

    Add widget for faq page

--- wp-content/themes/amedical/sidebar.php
+++ wp-content/themes/amedical/sidebar.php
@@ -8,5 +8,11 @@
 <div class="sixteen wide mobile six wide tablet five wide computer column" id="news-sidebar">
 	<div class="news-sidebar">
 		<?php dynamic_sidebar('main-sidebar'); ?>
+		<?php if(is_page_template('page-faq.php') && (get_field('not_found_link')) ) : ?>
+			<h3 class="ui header">
+				<?php the_field( 'not_found_title' ); ?>
+				<a class="ui primary button" href="<?php the_field( 'not_found_link' ); ?>"><?php the_field( 'not_found_text' ); ?></a>
+			</h3>
+		<?php endif; ?>
 	</div>
 </div>
