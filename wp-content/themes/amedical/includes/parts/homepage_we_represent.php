48eefe31c6e9cc7de8a19ebf518abfee699a9f80
Eugene <kudriashov.e@gmail.com>
2017-04-18 11:20:25 +0300

    Link to partners page added

--- wp-content/themes/amedical/includes/parts/homepage_we_represent.php
+++ wp-content/themes/amedical/includes/parts/homepage_we_represent.php
@@ -1,12 +1,10 @@
-
 <section class="represent ui container very padded">
     <h2 class="ui centered header centered decored">
         <?php the_field("we_represent_title"); ?>
     </h2>
     <div class="ui six column center aligned grid">
         <ul class="represent-slider">
-
-                <?php
+            <?php
                 $represent_params = array(
                     'post_type' => 'partner',
                     'posts_per_page' => -1
@@ -14,7 +12,6 @@
                 $represent_q= new WP_Query( $represent_params );
                 if ( $represent_q->have_posts() ) {
                     while ( $represent_q->have_posts() ) :
-
                         $represent_q->the_post();
                             $partner_logo = get_field('partner_logo');
                             ?>
@@ -22,18 +19,18 @@
                                 <img src="<?php echo $partner_logo['sizes']['brand-logo']; ?>">
                             </li>
                             <?php
-
                     endwhile;
-
-                    // Restore original Post Data
                     wp_reset_postdata();
                 }
                 ?>
         </ul>
-<!--        <div class="center aligned row">-->
-<!--            <a id="we-represent-button" class="ui primary button" href="--><?php //the_field("we_represent_link","option"); ?><!--">--><?php //the_field("we_represent_cta","option"); ?>
-<!--                <i class="long arrow right icon"></i>-->
-<!--            </a>-->
-<!--        </div>-->
+        <?php if(get_field('we_represent_link')) : ?>
+            <div class="center aligned row">
+                <a id="we-represent-button" class="ui primary button" href="<?php the_field('we_represent_link'); ?>">
+                    <?php the_field('we_represent_cta'); ?>
+                    <i class="long arrow right icon"></i>
+                </a>
+            </div>
+        <?php endif; ?>
     </div>
 </section>
