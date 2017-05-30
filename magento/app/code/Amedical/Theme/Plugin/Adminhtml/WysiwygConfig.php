1728667431ae0676e38a639a4ea94035de52729f
Evgen Slyusarenko <evgen.slyusarenko@gmail.com>
2017-05-30 14:58:14 +0300

    magento: add custom content.css to wysiwyg editor

--- magento/app/code/Amedical/Theme/Plugin/Adminhtml/WysiwygConfig.php
+++ magento/app/code/Amedical/Theme/Plugin/Adminhtml/WysiwygConfig.php
@@ -2,6 +2,15 @@
 namespace Amedical\Theme\Plugin\Adminhtml;
 
 class WysiwygConfig {
+
+    protected $_assetRepo;
+
+    public function __construct(
+        \Magento\Framework\View\Asset\Repository $assetRepo
+    ) {
+        $this->_assetRepo = $assetRepo;
+    }
+
     public function afterGetConfig($subject, \Magento\Framework\DataObject $config)
     {
         $styleArray = [
@@ -13,6 +22,7 @@ class WysiwygConfig {
         }, array_keys($styleArray), array_values($styleArray));
 
         $config->addData(["settings" => ["theme_advanced_styles" => implode("; ", $styles)]]);
+        $config->setData('content_css', $this->_assetRepo->getUrl('Amedical_Theme/css/wysiwyg/content.css'));
 
         return $config;
     }
