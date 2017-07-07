973a50a68f7cf8e87e3bda0678741ce1d7feb548
Aivars Lauzis <lauzis@inbox.lv>
2017-07-07 11:03:03 +0300

    clean cache script

---
+++ magento/clean_cache.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PHP_PATH="/usr/bin/php -d memory_limit=512M"
+MAGENTO_PATH=$DIR"/bin/magento"
+
+MODE="developer"
+
+echo $DIR
+echo $PHP_PATH
+echo $MAGENTO_PATH
+
+
+
+echo "Cache clean"
+echo "======================================"
+$PHP_PATH $MAGENTO_PATH cache:clean
+echo ""
+
+echo "Cache flush"
+echo "======================================"
+$PHP_PATH $MAGENTO_PATH cache:flush
+echo ""
+
+echo "Reindex"
+echo "======================================"
+$PHP_PATH $MAGENTO_PATH indexer:reindex
+echo ""
