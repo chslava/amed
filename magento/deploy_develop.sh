365ba41f6a7b07df3ca9b436747b7bd673c38557
Aivars Lauzis <lauzis@inbox.lv>
2017-07-07 10:54:17 +0300

    deploy skript update

--- magento/deploy_develop.sh
+++ magento/deploy_develop.sh
@@ -9,9 +9,15 @@ echo $DIR
 echo $PHP_PATH
 echo $MAGENTO_PATH
 
+
+echo "Enable maintenance mode"
+echo "======================================"
+$PHP_PATH $MAGENTO_PATH maintenance:enable
+echo ""
+
 echo "Removes cache folders"
 echo "======================================"
-rm -R $DIR"/var/di" $DIR"/var/generation" $DIR"/pub/static/frontend" $DIR"/pub/static/adminhtml"
+rm -R $DIR"/var/di" $DIR"/var/generation" $DIR"/pub/static/frontend"
 echo ""
 
 echo "Cache clean"
@@ -29,22 +35,30 @@ echo "======================================"
 $PHP_PATH $MAGENTO_PATH setup:upgrade
 echo ""
 
-echo "Compile"
-echo "======================================"
-$PHP_PATH $MAGENTO_PATH setup:di:compile
-echo ""
-
 echo "Reindex"
 echo "======================================"
 $PHP_PATH $MAGENTO_PATH indexer:reindex
 echo ""
 
-echo "Deploy"
-echo "======================================"
-$PHP_PATH $MAGENTO_PATH setup:static-content:deploy
-echo ""
 
 echo "Mode "$MODE
 echo "======================================"
-$PHP_PATH $MAGENTO_PATH deploy:mode:set $MODE
+$PHP_PATH $MAGENTO_PATH deploy:mode:set $MODE -s
+echo ""
+
+echo "Compile"
+echo "======================================"
+$PHP_PATH $MAGENTO_PATH setup:di:compile
+echo ""
+
+echo "Deploy"
+echo "======================================"
+$PHP_PATH $MAGENTO_PATH setup:static-content:deploy --theme=amedical/base ru_RU
+$PHP_PATH $MAGENTO_PATH setup:static-content:deploy --theme=amedical/base lv_LV
+$PHP_PATH $MAGENTO_PATH setup:static-content:deploy --theme=amedical/base en_US
+echo ""
+
+echo "Disable maintenance mode"
+echo "======================================"
+$PHP_PATH $MAGENTO_PATH maintenance:disable
 echo ""
