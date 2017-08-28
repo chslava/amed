5ea1c1b99faf3ee7668706ebdc0712fce2f61534
Aivars Lauzis <lauzis@inbox.lv>
2017-08-28 08:33:32 +0300

    helper added for theme related functions

---
+++ magento/app/code/Amedical/Theme/Helper/Data.php
@@ -0,0 +1,94 @@
+<?php
+namespace Amedical\Theme\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    /**
+     * @var \Magento\Catalog\Api\ProductAttributeRepositoryInterface
+     */
+    protected $attributeRepository;
+
+    /**
+     * @var array
+     */
+    protected $attributeValues;
+
+    /**
+     * @var \Magento\Eav\Model\Entity\Attribute\Source\TableFactory
+     */
+    protected $tableFactory;
+
+    /**
+     * @var \Magento\Eav\Api\AttributeOptionManagementInterface
+     */
+    protected $attributeOptionManagement;
+
+    /**
+     * @var \Magento\Eav\Api\Data\AttributeOptionLabelInterfaceFactory
+     */
+    protected $optionLabelFactory;
+
+    /**
+     * @var \Magento\Eav\Api\Data\AttributeOptionInterfaceFactory
+     */
+    protected $optionFactory;
+
+    /**
+     * Data constructor.
+     *
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Catalog\Api\ProductAttributeRepositoryInterface $attributeRepository
+     * @param \Magento\Eav\Model\Entity\Attribute\Source\TableFactory $tableFactory
+     * @param \Magento\Eav\Api\AttributeOptionManagementInterface $attributeOptionManagement
+     * @param \Magento\Eav\Api\Data\AttributeOptionLabelInterfaceFactory $optionLabelFactory
+     * @param \Magento\Eav\Api\Data\AttributeOptionInterfaceFactory $optionFactory
+     */
+    private $class = null;
+    private $store= null;
+    private $_objectManager = null;
+    
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Catalog\Api\ProductAttributeRepositoryInterface $attributeRepository,
+        \Magento\Eav\Model\Entity\Attribute\Source\TableFactory $tableFactory,
+        \Magento\Eav\Api\AttributeOptionManagementInterface $attributeOptionManagement,
+        \Magento\Eav\Api\Data\AttributeOptionLabelInterfaceFactory $optionLabelFactory,
+        \Magento\Eav\Api\Data\AttributeOptionInterfaceFactory $optionFactory
+    ) {
+        
+        
+        $this->class = explode('\\',__CLASS__);
+        $this->class = end($this->class);
+
+        $this->_objectManager=\Magento\Framework\App\ObjectManager::getInstance();
+        $this->store = $this->_objectManager->create('DS\Hanza\Helper\Store');
+
+        parent::__construct($context);
+        
+    }
+
+    public function get_contact_url(){
+        $curent_store_data = $this->store->get_current_store_data();
+        $base_url = $this->store->get_base_url();
+        $base_url = dirname(dirname($base_url));
+
+        switch($curent_store_data['store_code']){
+            case "lv":
+                return $base_url."/par-mums/kontakti/";
+                break;
+            case "ru":
+                return $base_url."/ru/ob-a-medical/kontakty/";
+                break;
+            case "en":
+                return $base_url."v/en/about-a-medical/contacts/";
+                break;
+        }
+
+        return "";
+    }
+
+    public function get_current_url(){
+        return (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    }
+    
+}
