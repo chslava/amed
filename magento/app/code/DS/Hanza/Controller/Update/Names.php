52d41fc5e348aa4690b4dc668e5029ced2bddf85
Aivars Lauzis <lauzis@inbox.lv>
2017-08-28 14:05:00 +0300

    cahce and store helpers added + changed the script to skipp those that does not need update

--- magento/app/code/DS/Hanza/Controller/Update/Names.php
+++ magento/app/code/DS/Hanza/Controller/Update/Names.php
@@ -22,6 +22,8 @@ class Names extends \Magento\Framework\App\Action\Action
 
         $this->helper = $this->_objectManager->create('DS\Hanza\Helper\Data');
         $this->products = $this->_objectManager->create('DS\Hanza\Helper\Products');
+        $this->store = $this->_objectManager->create('DS\Hanza\Helper\Store');
+        $this->cache = $this->_objectManager->create('DS\Hanza\Helper\Cache');
         
 
         parent::__construct($context);
@@ -42,29 +44,62 @@ class Names extends \Magento\Framework\App\Action\Action
             $counter++;
             
             $prod_data= $this->helper->get_product_data($sku);
+
             if(!$prod_data){
                 print("$counter : Skipped product (there is no product data $sku) name :".$product->getName()." (".$product->getId()."/".$product->getSku().") <br/>");
                 $this->products->disable($sku,$id);
                 continue;
+            }else {
+                $this->products->enable($sku,$id);
             }
             
             //TODO Update code
-          
-            
+
             print("$counter : Updating prduct name for:".$product->getName()." (".$product->getId()."/".$product->getSku().") <br/>");
-    
-            $languages = ["lv","en","ru"];
-            foreach($languages as $lang){
-                
-                $result = $this->products->add_language($product->getId(), $prod_data["name"],$lang,$sku);
-                if ($result["status"]){
-                    print("Updated: $lang : ".$prod_data["name"]."<br/>");
-                } else {
-                    print("Failed: $lang : ".$prod_data["name"]."<br/>");
+
+            $all_websites_ids = null;
+            $magento_sores=$this->helper->store->get_magento_stores();
+
+            $magento_sores['default']=[
+                'storeId'=>0,
+                'websiteId'=>$all_websites_ids
+            ];
+            $failed=0;
+            $update=0;
+
+            foreach($magento_sores as $store){
+
+
+                $_product=null;
+                $sid = $store["storeId"];
+                $_product = $this->_objectManager->create('Magento\Catalog\Model\Product')->setStoreId($sid)->load($product->getId());
+                $sku = $_product->getSku();
+                $prod_store_id = $_product->getStoreId();
+                $product_name = $_product->getName();
+                $new_product_name = trim(str_replace($sku,"",$product_name));
+                if ($new_product_name==$product_name){
+                    print("skipped no need to update<br/>");
+                    continue;
                 }
+
+                $_product->setName($new_product_name);
+                $_product->setStoreId($sid);
+
+                try {
+                    print("<strong>updated</strong><br/>");
+                    $_product->save();
+                    $this->cache->set_cache_data($product->getSku(),true,"names_updated");
+                    $updated++;
+                } catch (\Exception $e) {
+                    $this->cache->set_cache_data($product->getSku(),false,"names_updated");
+                    $failed++;
+                    print("<strong style='color:red'>".$counter." : ".$_product->getId()." could not save. </strong><br/>");
+
+                }
+
             }
-            
-        
+
+
         }
         
     }
