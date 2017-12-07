16499c4735a375c2d5fa853f1e4959f48d0254a2
Aivars Lauzis <lauzis@inbox.lv>
2017-12-07 13:19:48 +0200

    compare store views

--- magento/app/code/DS/Importer/Controller/Update/Checkcategoryvisibility.php
+++ magento/app/code/DS/Importer/Controller/Update/Checkcategoryvisibility.php
@@ -27,29 +27,32 @@ class Checkcategoryvisibility extends \Magento\Framework\App\Action\Action
     
     public function execute()
     {
-        $categoryFactory = $this->_objectManager->create('Magento\Catalog\Model\ResourceModel\Category\CollectionFactory');
-        $categories = $categoryFactory->create()
-            ->setStoreId(1)
-            ->addAttributeToSelect('*');
+        foreach ([0,1,2,3] as $store_id){
+            $categoryFactory = $this->_objectManager->create('Magento\Catalog\Model\ResourceModel\Category\CollectionFactory');
+            $categories = $categoryFactory->create()
+                ->setStoreId($store_id)
+                ->addAttributeToSelect('*');
+            print("<h1>$store_id</h1>");
+            //categories from current store will be fetched
+            foreach($categories as $cat){
+                $c = $this->_objectManager->create('Magento\Catalog\Model\Category')->setStoreId(1)->load($cat->getId());
+                if (!$c->getData("include_in_menu")){
+                    print("include in menu:");
+                    var_dump($c->getData("include_in_menu"));
 
-        //categories from current store will be fetched
-        foreach($categories as $cat){
-            $c = $this->_objectManager->create('Magento\Catalog\Model\Category')->setStoreId(1)->load($cat->getId());
-            if (!$c->getData("include_in_menu")){
-                print("include in menu:");
-                var_dump($c->getData("include_in_menu"));
 
 
+                    print(" name:");
+                    print($c->getData("name"));
 
-                print(" name:");
-                print($c->getData("name"));
+                    print(" id:");
+                    print($c->getId());
+                    print("<br/>");
+                }
 
-                print(" id:");
-                print($c->getId());
-                print("<br/>");
             }
-
         }
+
     }
 
 }
