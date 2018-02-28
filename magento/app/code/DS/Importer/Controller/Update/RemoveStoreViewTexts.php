057f573cb015f6555aef3e0106bac9925b703312
Aivars Lauzis <lauzis@inbox.lv>
2018-02-28 11:41:06 +0200

    lets resave again

--- magento/app/code/DS/Importer/Controller/Update/RemoveStoreViewTexts.php
+++ magento/app/code/DS/Importer/Controller/Update/RemoveStoreViewTexts.php
@@ -33,7 +33,6 @@ class RemoveStoreViewTexts extends \Magento\Framework\App\Action\Action
     
     public function execute()
     {
-        die("disabled");
         $stores = $this->store->get_magento_stores();
 
         $productCollection = $this->_objectManager->create('Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
